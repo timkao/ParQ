9ed6677eeff4a16df5c156fb28604048bb64ebb3
AUNGKYAWCHA MARMA <aungkyawcha@AUNGKYAWCHAs-MacBook-Pro.local>
2017-11-09 19:57:36 -0500

    added rank instance method in reward model

--- server/db/models/reward.js
+++ server/db/models/reward.js
@@ -6,9 +6,27 @@ const Reward = db.define("reward", {
           type : Sequelize.INTEGER,
           defaultValue : 0
       }
-})
+});
 
+// get user rankig based on the number of points
+const rankings = (points)=>{
+  let rank = 'Just a happy member';
+  if(points >= 30){
+      rank = "Gold: helped more than  30 people find parking spots."
+  }
+  else if(points >= 20 && points < 30){
+      rank = "Premium:  helped more than  20 people find parking spots."
+  }
+  else if(points >= 10 && points < 20){
+      rank = "Almost a Premium member: helped more than 10 people find parking spots."
+  }
+  else if(points >= 1 && points < 10){
+      rank = "love helpig people find parking spots."
+  }
+  return rank;
+}
 Reward.prototype.rank = function(){
+    return rankings(this.points);
 }
 
 module.exports = Reward;
