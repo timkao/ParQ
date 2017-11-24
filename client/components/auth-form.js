8690c046b10b6841af7bc5763a553cefdbcf1ab9
Tim Kao <pakuya.kao@gmail.com>
2017-11-24 13:32:42 -0500

    sign up validation done

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { auth } from '../store'
 
@@ -7,7 +7,8 @@ import { auth } from '../store'
  * COMPONENT
  */
 const AuthForm = (props) => {
-  const {name, displayName, handleSubmit, error} = props
+  const { name, displayName, handleSubmit, error, handleFocus, handleBlur, handleKeyup } = props
+
 
   return (
     <div id="auth">
@@ -18,31 +19,28 @@ const AuthForm = (props) => {
         </div>
         <div className="form-group">
           <label htmlFor='psw'><small>Password</small></label>
-          {/* <input name='password' type='password' /> */}
-          <input className="form-control" type="password" id="psw" name="psw" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required />
+          {displayName == 'Sign Up' ?
+            <input className="form-control" type="password" id="psw" name="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" onFocus={handleFocus} onBlur={handleBlur} onKeyUp={handleKeyup} required />
+            : <input className="form-control" type="password" id="psw" name="password" required />
+          }
         </div>
         <div className="form-group">
-          {/* <button type='submit'>{displayName}</button> */}
           <input id="form-submit" className="btn btn-default" type='submit' value={displayName} />
           <a id="auth-google" href='/auth/google'>Login with Google</a>
         </div>
         {error && error.response && <div className="form-group alert alert-danger"> {error.response.data} </div>}
-        {/* <div className="form-group">
-          <a href='/auth/google'>Login with Google</a>
-        </div> */}
       </form>
-      {/* <a href='/auth/google'>Login with Google</a> */}
+      <div id="message">
+        <h3>Password must contain the following:</h3>
+        <p id="letter" className="invalid">A <b>lowercase</b> letter</p>
+        <p id="capital" className="invalid">A <b>capital (uppercase)</b> letter</p>
+        <p id="number" className="invalid">A <b>number</b></p>
+        <p id="length" className="invalid">Minimum <b>8 characters</b></p>
+      </div>
     </div>
   )
 }
 
-/**
- * CONTAINER
- *   Note that we have two different sets of 'mapStateToProps' functions -
- *   one for Login, and one for Signup. However, they share the same 'mapDispatchToProps'
- *   function, and share the same Component. This is a good example of how we
- *   can stay DRY with interfaces that are very similar to each other!
- */
 const mapLogin = (state) => {
   return {
     name: 'login',
@@ -60,13 +58,67 @@ const mapSignup = (state) => {
 }
 
 const mapDispatch = (dispatch, ownProps) => {
+  // pending question to prof...
+  /*
+    Following two lines get the element but when i use them to update elements
+    they do not work...
+    const message = document.getElementById('message');
+    const letter = document.getElementById("letter");
+  */
+
   return {
-    handleSubmit (evt) {
+    handleSubmit(evt) {
       evt.preventDefault()
       const formName = evt.target.name
       const email = evt.target.email.value
       const password = evt.target.password.value
       dispatch(auth(email, password, formName, ownProps.history))
+    },
+    handleFocus(evt) {
+      evt.preventDefault();
+      document.getElementById("message").style.display = "block";
+    },
+    handleBlur(evt) {
+      evt.preventDefault();
+      document.getElementById("message").style.display = "none";
+    },
+    handleKeyup(evt) {
+      evt.preventDefault();
+      // validate lower case
+      const lowerCaseLetters = /[a-z]/g;
+      if (evt.target.value.match(lowerCaseLetters)) {
+        document.getElementById('letter').classList.remove("invalid");
+        document.getElementById('letter').classList.add("valid");
+      } else {
+        document.getElementById('letter').classList.remove("valid");
+        document.getElementById('letter').classList.add("invalid");
+      }
+      // validate upper case
+      const upperCaseLetters = /[A-Z]/g;
+      if (evt.target.value.match(upperCaseLetters)) {
+        document.getElementById('capital').classList.remove("invalid");
+        document.getElementById('capital').classList.add("valid");
+      } else {
+        document.getElementById('capital').classList.remove("valid");
+        document.getElementById('capital').classList.add("invalid");
+      }
+      // Validate numbers
+      const numbers = /[0-9]/g;
+      if (evt.target.value.match(numbers)) {
+        document.getElementById('number').classList.remove("invalid");
+        document.getElementById('number').classList.add("valid");
+      } else {
+        document.getElementById('number').classList.remove("valid");
+        document.getElementById('number').classList.add("invalid");
+      }
+      // Validate length
+      if (evt.target.value.length >= 8) {
+        document.getElementById('length').classList.remove("invalid");
+        document.getElementById('length').classList.add("valid");
+      } else {
+        document.getElementById('length').classList.remove("valid");
+        document.getElementById('length').classList.add("invalid");
+      }
     }
   }
 }
