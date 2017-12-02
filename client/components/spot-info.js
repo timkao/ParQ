e303c532081055ac299501718e7a0a81888ec37e
Mike Kerslake <theonlymikeever@gmail.com>
2017-12-02 09:57:36 -0500

    Cleanup

--- client/components/spot-info.js
+++ client/components/spot-info.js
@@ -1,11 +1,10 @@
 import React, { Component} from 'react';
 import Moment from 'react-moment';
 import 'moment-timezone';
-import { timer, timeSince } from '../helpers';
 
 class SpotInfo extends Component {
   render(){
-  const { spot, map, handleTakeSpot } = this.props;
+  const { spot, handleTakeSpot } = this.props;
   const { createdAt } = spot.properties
 
   return (
