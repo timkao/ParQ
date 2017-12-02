fa7e7ef162a885c9e4d580e024dd5559c817d2a0
Tim Kao <pakuya.kao@gmail.com>
2017-12-02 11:58:26 -0500

    meter almost done

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { takeSpot, updateSpotsTaken, addSpotOnServer, updateUserPoints } from '../store';
+import { takeSpot, updateSpotsTaken, addSpotOnServer, updateUserPoints, getNotification } from '../store';
 import socket from '../socket';
 import Map from './Map';
 import List from './List';
@@ -42,18 +42,10 @@ export class UserHome extends Component {
 
   componentDidMount() {
     socket.on('notifications', message => {
-      //this.props.showMeter();
       const meter = document.getElementById("meter");
       meter.className = "animated slideInRight";
       meter.style.display = "block";
-      setTimeout(function () { this.props.gainedPoints() }.bind(this), 1000);
-
-      // update point
-
-      // this.setState({showNotification: {isShow: true, message: message}});
-      // setTimeout(() => {
-      //   this.setState({ showNotification: {isShow: false, message: ''}});
-      // }, 4000);
+      setTimeout(function () { this.props.gainedPoints(1) }.bind(this), 1000);
     });
   }
 
@@ -76,7 +68,6 @@ export class UserHome extends Component {
 
   handleTest() {
     console.log('-----testing only-----');
-    //this.props.showMeter();
     const meter = document.getElementById("meter");
     meter.className = "animated slideInRight";
     meter.style.display = "block";
@@ -136,15 +127,10 @@ const mapDispatch = (dispatch, ownProps) => {
     updateUserSpotsTaken(comp, spots) {
       dispatch(updateSpotsTaken())
         .then(() => {
-          // comp.setState({
-          //   showNotification: { isShow: true, message: `${spots} spot${spots > 1 ? 's' : ''} you reported ${spots > 1 ? 'are' : 'is'} taken! You earned ${spots * 100} points` }
-          // });
-          // setTimeout(() => {
-          //   comp.setState({ showNotification: { isShow: false, message: '' } });
-          // }, 4000);
           const meter = document.getElementById("meter");
           meter.className = "animated slideInRight";
           meter.style.display = "block";
+          dispatch(getNotification(`${spots} Taken! You got ${spots * 100} points`))
           setTimeout(function () {dispatch(updateUserPoints(spots))}, 1000);
         })
     },
@@ -154,10 +140,8 @@ const mapDispatch = (dispatch, ownProps) => {
     toReportForm() {
       ownProps.history.push('/home/reportForm');
     },
-    showMeter() {
-      dispatch(getIsShow(true));
-    },
     gainedPoints(num) {
+      dispatch(getNotification('A post is taken! Yout got 100 points'));
       dispatch(updateUserPoints(num));
     }
   };
