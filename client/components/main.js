d82c3ef0bf20c1d2e4428acaeda164a89fe2c4fc
AUNGKYAWCHA MARMA <aungkyawcha@AUNGKYAWCHAsMBP.home>
2017-12-01 20:46:16 -0500

    almost done styling of profile page

--- client/components/main.js
+++ client/components/main.js
@@ -24,7 +24,7 @@ const Main = (props) => {
               {/* The navbar will show these NavLinks after you log in */}
               <Link to='/home'>Home</Link>
               <Link to={`/profile/${1}`} replace>      {/* replace to use if Link is being used as NavLink */}
-                <img style={{ width: "5%"}} className="pull-right image-responsive" src="https://assets-cdn.github.com/images/modules/open_graph/github-mark.png" />
+                <img style={{ width: "10%"}} className="pull-right image-responsive" src="https://assets-cdn.github.com/images/modules/open_graph/github-mark.png" />
               </Link>
               <a href='#' onClick={handleClick}>Logout</a>
             </div>
