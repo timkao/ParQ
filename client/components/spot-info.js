4cbf471fc5d75baae111dbb486064c97d977e634
Mike Kerslake <theonlymikeever@gmail.com>
2017-12-03 11:50:15 -0500

    Modal btn, modal and styling

--- client/components/spot-info.js
+++ client/components/spot-info.js
@@ -1,21 +1,26 @@
 import React, { Component} from 'react';
 import Moment from 'react-moment';
 import 'moment-timezone';
+import { createImgModal } from '../helpers';
 
 class SpotInfo extends Component {
   render(){
   const { spot, handleTakeSpot, handleNavigate } = this.props;
-  const { createdAt } = spot.properties
+  const { images, createdAt } = spot.properties;
+  const imgSrc = images[0] || null;
 
   return (
        <div>
          <div>
-          <strong>Reported</strong><br/>
+          <strong>Reported</strong><br />
           <span className="time"><Moment fromNow>{createdAt}</Moment></span>
           </div>
           <div className="spot-actions">
-          <button className="spot-btn-nav" onClick={handleNavigate}>NAVIGATE TO</button>
-          <button className="spot-btn-take" onClick={handleTakeSpot}>TAKE SPOT</button>
+            <button className="spot-btn-img" onClick={() => createImgModal(imgSrc)}><span className="glyphicon glyphicon-picture" aria-hidden="true" />SEE IMAGE</button>
+            <button className="spot-btn-nav" onClick={handleNavigate}><span className="glyphicon glyphicon-road" aria-hidden="true" />NAVIGATE TO</button>
+          </div>
+          <div className="spot-actions">
+            <button className="spot-btn-take" onClick={handleTakeSpot}><span className="glyphicon glyphicon-thumbs-up" aria-hidde="true" />TAKE SPOT</button>
           </div>
        </div>
     )
