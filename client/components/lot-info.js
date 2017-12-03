eeb8b202096827f2b63c8abe2e7ea8fff2a7a2c8
Mike Kerslake <theonlymikeever@gmail.com>
2017-12-03 10:58:30 -0500

    cleanup code

--- client/components/lot-info.js
+++ client/components/lot-info.js
@@ -4,9 +4,6 @@ class LotInfo extends Component {
   render(){
   const { lot, handleNavigate } = this.props;
   const { spotsAvailable } = lot.properties;
-  console.log('lot:',lot)
-  console.log('descript:',Object.getOwnPropertyDescriptor(lot, "place_name"))
-  console.log('place_name:', lot["place_name"])
   return (
    <div>
      <div>
